ec0cf323df30d087d0db1fe1687205be04d822dc
sharifahmad <sharifahmad2061@gmail.com>
2021-07-23 11:03:57 +0500

    changed the input form

--- RouteTracker/forms.py
+++ RouteTracker/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
+from django.contrib.postgres.forms import SplitArrayField
 from django import forms
 from .models import Route
 
 
-class RouteForm(ModelForm):    
+class RouteForm(ModelForm):
+    departure = SplitArrayField(forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime-local'})), 20, remove_trailing_nulls=True)
+    transit = SplitArrayField(forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime-local'})), 20, remove_trailing_nulls=True)
+    arrival = SplitArrayField(forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime-local'})), 20, remove_trailing_nulls=True)
+    stay = SplitArrayField(forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime-local'})), 20, remove_trailing_nulls=True)
+
     class Meta:
         model = Route
         verbose_name = "Route List"
